e6beba2ef72684c800a53901c4350c67c4ec9ce7
不爱打代码的程序员 <2284609302@qq.com>
2024-06-11 20:58:37 +0800

    提交备案图片

--- themeHope/.vuepress/theme.ts
+++ themeHope/.vuepress/theme.ts
@@ -43,8 +43,8 @@ export default hopeTheme({
   // 页脚
   // 页脚支持http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=41030502000411
   footer: '<a href="https://beian.miit.gov.cn/" target="_blank">豫ICP备20230606088</a>'
-      +'<img src="https://cdn.tobebetterjavaer.com/tobebetterjavaer/images/beian.png" height="15px" width="15px" />'
-      +'<a target="_blank" href="https://vscodepic.oss-cn-beijing.aliyuncs.com/blog/202406112054052.png">'
+      +'<img src="https://vscodepic.oss-cn-beijing.aliyuncs.com/blog/202406112054052.png" height="15px" width="15px" />'
+      +'<a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=41030502000411">'
       +'<span>豫公网安备 41080552980273号</span>'
       +'</a>',
   displayFooter: true,
